924047ad036d841300d6e09c377eef437516755d
pirox07 <pirox.07@gmail.com>
2021-01-01 01:20:10 +0900

    [Update]HTML ファイルのテンプレード化に対応

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -1,23 +1,32 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
 
-func main(){
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>チャット</title>
-				</head>
-				<body>
-					チャットしましょう！
-				</body>
-			</html>
-		`))
+type templateHandler struct {
+	once sync.Once
+	filename string
+	temp1 *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.temp1 =
+				template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	t.temp1.Execute(w, nil)
+}
+
+
+func main(){
+
+	// root
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	// Start Web Server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
