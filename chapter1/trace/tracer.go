b302387601ffeaa351c5ace3f8edc4ee45c7cf99
pirox07 <pirox.07@gmail.com>
2021-04-19 00:22:22 +0900

    [add] ロギングツールの作成、ユニットテストの準備

---
+++ chapter1/trace/tracer.go
@@ -0,0 +1,8 @@
+package trace
+
+
+// Tracer is the interface that describes an object capable of
+// tracing events throughout code.
+type Tracer interface {
+	Trace(...interface{})
+}
---
+++ chapter1/trace/tracer_test.go
@@ -0,0 +1,7 @@
+package trace
+
+import "testing"
+
+func TestNew(t *testing.T){
+	t.Error("There's no test yet.")
+}
