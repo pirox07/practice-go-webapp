47e978460f45ad7629564dd401327004243a8e58
pirox07 <pirox.07@gmail.com>
2021-04-18 23:56:16 +0900

    [fix] http.request の内容を参照できるように修正

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -11,26 +12,31 @@ import (
 type templateHandler struct {
 	once sync.Once
 	filename string
-	temp1 *template.Template
+	templ *template.Template
 }
 
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
-		t.temp1 =
+		t.templ =
 				template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.temp1.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 
 func main(){
-
-	// root
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
 
-	// Start Web Server
+	// get the room going
+	go r.run()
+
+	// start the web server
+	log.Println("Starting web server on :", *addr);
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
-
